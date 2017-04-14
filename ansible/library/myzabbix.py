a863ced4374749607f47639f063d4b6bf275558a
Vladimir Krumshtein <vladimir.krumstein@clustervision.com>
2017-04-14 10:46:31 +0200

    Added find_update action on ansible, updated example yaml

--- ansible/library/myzabbix.py
+++ ansible/library/myzabbix.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python
 
-from  ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.pycompat24 import get_exception
 import json
-import datetime
-import logging
-import copy
-import pprint
-try:
-    from zabbix_api import ZabbixAPI, ZabbixAPISubClass
+from zabbix_api import ZabbixAPI, ZabbixAPISubClass, ZabbixAPIException
 
-    HAS_ZABBIX_API = True
-except ImportError:
-    HAS_ZABBIX_API = False
 
 
 def main():
@@ -23,18 +15,32 @@ def main():
             hostname      = dict(required=True),
             object        = dict(required=True),
             action        = dict(required=True),
-            params        = dict(default={},type="dict" )
+            params        = dict(default={},type="dict" ),
+            query         = dict(type="dict"),
             )
     )
-    date = str(datetime.datetime.now())
-
-    zapi = ZabbixAPI('http://{}/zabbix'.format(module.params['hostname'] ))
-
-    zapi.login(module.params['username'], module.params['password'])
-    object = getattr(zapi, module.params['object'])
-    method = getattr(object, module.params['action'])
-    result =  method(module.params['params'])
-    module.exit_json(changed=True, result=result)
+    try:
+        zapi = ZabbixAPI('http://{}/zabbix'.format(module.params['hostname'] ))
+        zapi.login(module.params['username'], module.params['password'])
+        object = getattr(zapi, module.params['object'])
+        result=[]
+        msg = []
+        if module.params['action'] == "find_update":        
+            found_objects = object.get({'filter':module.params['query']})
+            msg.append(found_objects)
+            if len(found_objects) == 0 :
+                module.fail_json("Could not find object")
+            else:
+                #map(lambda x: result.append(object.update(x.update(module.params['params']))),found_objects)
+                #map(lambda x: result.append(dict({k:v for k,v in x.items() if k.endswith('id')} , **module.params['params'])),found_objects)
+                map(lambda x: result.append(object.update(dict({k:v for k,v in x.items() if k.endswith('id')}, **module.params['params']))),found_objects)
+    
+        else:
+            method = getattr(object, module.params['action'])
+            result =  method(module.params['params'])
+        module.exit_json(changed=True, result=result,msg=msg)
+    except ZabbixAPIException as e:
+        module.fail_json(msg="Zabbix API exception:{}".format(e))
  
 if __name__ == '__main__':  
     main()
